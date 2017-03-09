f99516f3218cfe1a2f4eae1e3595ebbe202d28c8
SinghDhanvinder <SinghDhanvinder@users.noreply.github.com>
2017-03-09 18:11:09 -0500

    Update logTime.py

--- code/logTime.py
+++ code/logTime.py
@@ -4,16 +4,15 @@ import sqlite3 as mydb
 import sys
 
 def logTime():
-	xTime = time.strftime('%Y-%m-%d')
-	xDate = time.strftime('%H-%M-%S')
+	xTime = time.strftime('%Y-%m-%d') #this gives us year month and date
+	xDate = time.strftime('%H-%M-%S') #this gives us Hour Min and Sec
 	list1 = [xTime, xDate]
 
 	con = mydb.connect('testTime.db')
 	with con:
 		try:
 			cur = con.cursor()
-			sql = "insert into TempData values(?,?)"
-			cur.execute('insert into testTime values(?,?)',list1)
+			cur.execute('insert into testTime values(?,?)',list1) #writes time and date into dataBase
 			print("Time Logged")
 		except:
 			print("Error!!")
